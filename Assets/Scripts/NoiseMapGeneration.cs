7f13e467f75802925b61d9aa3f6ea5394b506a3c
JordyPirata <103045128+JordyPirata@users.noreply.github.com>
2022-09-01 21:45:17 -0500

    NoiseGeneratorScript
    Added NoiseGeneratorScript and the prefab plane

--- Assets/Scripts/NoiseMapGeneration.cs
+++ Assets/Scripts/NoiseMapGeneration.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class NoiseMapGeneration : MonoBehaviour
 {
-    public float[,] GenerateNoisemap(int mapWidth, float scale)
+    public float[,] GenerateNoisemap(int mapDepth, int mapWidth, float scale, float offsetX, float offsetZ)
     {
         float[,] noiseMap = new float[mapDepth, mapWidth];
 
-        
+        for(int zIndex = 0; zIndex < mapDepth; zIndex++)
+        {
+            for(int xIndex = 0;xIndex < mapWidth; xIndex++)
+            {
+                float sampleX = (xIndex + offsetX) / scale;
+                float sampleZ = (zIndex + offsetZ) / scale;
+
+                float noise = Mathf.PerlinNoise(sampleX,sampleZ);
+
+                noiseMap[zIndex, xIndex] = noise;
+            }
+        }
         
         return noiseMap;
     }
