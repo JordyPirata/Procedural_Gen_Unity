ee4ecbc6f6003b2d6e73943db0aed624eb2b0eea
JordyPirata <103045128+JordyPirata@users.noreply.github.com>
2022-09-27 09:49:02 -0500

    Se agregó el sistema de semillas

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -8,17 +8,46 @@ namespace MapGeneration
         public float noiseScale;
 
         public int octaves;
+        [Range(0,1)]
         public float persistance;
         public float lacunarity;
+
+        public int seed;
+        public Vector2 offset;
+
 
         public bool AutoUpdate;
 
         public void GenerateMap() 
         { 
-            float[,] noiseMap = NoiseMapGeneration.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, octaves, persistance, lacunarity);
+            float[,] noiseMap = NoiseMapGeneration.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
             MapDisplay display = FindObjectOfType<MapDisplay>();
             display.DrawNoiseMap(noiseMap);
         }
+        private void OnValidate()
+        {
+            if (mapWidth < 1)
+            {
+                mapWidth = 1;
+            }
+            if (mapHeight < 1)
+            {
+                mapHeight = 1;
+            }
+            if(lacunarity < 1)
+            {
+                lacunarity = 1;
+            }
+            if(octaves < 0)
+            {
+                octaves = 0;
+            }
+
+        }
+    }
+    public struct TerrainType
+    {
+
     }
 }
--- Assets/Scripts/NoiseMapGeneration.cs
+++ Assets/Scripts/NoiseMapGeneration.cs
@@ -1,14 +1,22 @@
 using System;
-using System.Collections.Generic;
+using Random = System.Random;
 using UnityEngine;
 
 namespace MapGeneration
 {
     public static class NoiseMapGeneration
     {
-        public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int octaves, float persistance, float lacunarity)
+        public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight,int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
         {
             float[,] noiseMap = new float[mapWidth, mapHeight];
+            Random prng = new Random(seed);
+            Vector2[] octaveOffsets = new Vector2[octaves];
+            for (int i = 0; i < octaves; i++)
+            {
+                float offsetY = prng.Next(-100000,100000) + offset.x;
+                float offsetX = prng.Next(-100000,100000) + offset.y;
+                octaveOffsets[i] = new Vector2(offsetX, offsetY);
+            }
             
             if (scale <= 0)
             {
@@ -17,6 +25,10 @@ namespace MapGeneration
 
             float maxNoiseHeight = float.MinValue;
             float minNoiseHeight = float.MaxValue;
+
+            float halfWidth = mapWidth / 2f;
+            float halfHeight = mapHeight / 2f;
+
             for (int yIndex = 0; yIndex < mapHeight; yIndex++)
             {
                 for (int xIndex = 0; xIndex < mapWidth; xIndex++)
@@ -27,8 +39,8 @@ namespace MapGeneration
 
                     for (int i = 0; i < octaves; i++)
                     { 
-                        float sampleX = xIndex / scale * frequency;
-                        float sampleY = yIndex / scale * frequency; 
+                        float sampleX = (xIndex-halfWidth) / scale * frequency + octaveOffsets[i].x;
+                        float sampleY = (yIndex- halfHeight) / scale * frequency + octaveOffsets[i].y; 
 
                         float noise = Mathf.PerlinNoise(sampleX, sampleY) * 2 -1;
                         noiseHeight += noise * amplitude;
