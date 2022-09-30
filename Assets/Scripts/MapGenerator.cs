a7dd813e7b6da89fcebdfd4fde0c6fd0c19c0467
JordyPirata <103045128+JordyPirata@users.noreply.github.com>
2022-09-30 14:43:22 -0500

    Se agregó TextureGenerator
    Con esto se pudo obtener una distribución de la tierra de acuerdo a la altura

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,8 +1,18 @@
 ﻿using UnityEngine;
 namespace MapGeneration
 {
+    [System.Serializable]
+    public class TerrainType
+    {
+        public string name;
+        public float height;
+        public Color color;
+    }
     public class MapGenerator : MonoBehaviour
     {
+        public enum DrawMode {NoiseMap, ColorMap}
+        public DrawMode drawMode;
+
         public int mapWidth;
         public int mapHeight;
         public float noiseScale;
@@ -15,6 +25,8 @@ namespace MapGeneration
         public int seed;
         public Vector2 offset;
 
+        [SerializeField]
+        private TerrainType[] terrainTypes;
 
         public bool AutoUpdate;
 
@@ -22,8 +34,39 @@ namespace MapGeneration
         { 
             float[,] noiseMap = NoiseMapGeneration.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+            Color[] colorMap = new Color[mapWidth * mapHeight];
+            for (int yIndex = 0; yIndex < mapWidth; yIndex++)
+            {
+                for (int xIndex = 0; xIndex < mapWidth; xIndex++)
+                {
+                    int colorIndex = yIndex * mapHeight + xIndex;
+                    float height = noiseMap[yIndex, xIndex];
+
+                    TerrainType terrainType = ChooseTerrainType(height);
+                    colorMap[colorIndex] = terrainType.color;
+                }
+            }
             MapDisplay display = FindObjectOfType<MapDisplay>();
-            display.DrawNoiseMap(noiseMap);
+            if (drawMode == DrawMode.NoiseMap)
+            {
+                display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+            }
+            else if (drawMode == DrawMode.ColorMap)
+            {
+                display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+            }
+        }
+
+        TerrainType ChooseTerrainType(float height)
+        {
+            foreach (TerrainType terrainType in terrainTypes)
+            {
+                if (height > terrainType.height)
+                {
+                    return terrainType;
+                }
+            }
+            return terrainTypes[terrainTypes.Length - 1];
         }
         private void OnValidate()
         {
@@ -45,9 +88,5 @@ namespace MapGeneration
             }
 
         }
-    }
-    public struct TerrainType
-    {
-
     }
 }
---
+++ Assets/Scripts/TextureGenerator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace MapGeneration
+{
+    public static class TextureGenerator
+    {
+        public static Texture2D TextureFromColorMap(Color[] colourMap, int width, int height)
+        {
+
+            Texture2D texture = new Texture2D(width, height);
+
+            texture.filterMode = FilterMode.Point;
+            texture.wrapMode = TextureWrapMode.Clamp;
+            texture.SetPixels(colourMap);
+            texture.Apply();
+
+            return texture;
+        }
+        public static Texture2D TextureFromHeightMap(float[,] noise)
+        {
+            int width = noise.GetLength(0);
+            int heigth = noise.GetLength(1);
+
+            Color[] colorMap = new Color[width * heigth];
+            for (int y = 0; y < heigth; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, noise[x, y]);
+                }
+            }
+            return TextureFromColorMap(colorMap,width,heigth);
+        }
+    }
+}
