30416700e4cc85003d8fb9bd53fb0006933b7249
JordyPirata <103045128+JordyPirata@users.noreply.github.com>
2022-10-07 23:27:38 -0500

    Se agregaron las instancias delos planos

--- Assets/Scripts/MapGeneration/EndlessTerrain.cs
+++ Assets/Scripts/MapGeneration/EndlessTerrain.cs
@@ -13,6 +13,7 @@ namespace MapGeneration
         int chunkVisibleInViewDst;
 
         Dictionary<Vector2, TerrainChunk> terrainChunkDiccionary = new Dictionary<Vector2, TerrainChunk>();
+        List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
 
         void Start()
         {
@@ -20,7 +21,11 @@ namespace MapGeneration
             chunkVisibleInViewDst = Mathf.RoundToInt(maxViewDst / chunkSize);
 
         }
-
+        private void Update()
+        {
+            viwerPosition = new Vector2(viewer.position.x,viewer.position.z);
+            UpdateVisibleChunks();
+        }
         void UpdateVisibleChunks()
         {
             int currentChunkCoordX = Mathf.RoundToInt(viwerPosition.x / chunkSize);
@@ -31,13 +36,44 @@ namespace MapGeneration
                 for (int xOffset = -chunkVisibleInViewDst; xOffset <= chunkVisibleInViewDst; xOffset++)
                 {
                     Vector2 viewedChunkCoord = new Vector2(currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
+
+                    if (terrainChunkDiccionary.ContainsKey(viewedChunkCoord))
+                    {
+                        terrainChunkDiccionary[viewedChunkCoord].UpdateTerrainChunk();
+                    }
+                    else
+                    {
+                        terrainChunkDiccionary.Add(viewedChunkCoord, new TerrainChunk(viewedChunkCoord, chunkSize));
+                    }
                 }
             }
         }
         public class TerrainChunk
         {
+            GameObject meshObject;
+            Vector2 position;
+            Bounds bounds;
+            public TerrainChunk(Vector2 coord, int size)
+            {
+                position = coord * size;
+                bounds = new Bounds(position,Vector2.one * size);
+                Vector3 positionV3 = new Vector3(position.x,0,position.y);
 
-
+                meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                meshObject.transform.position = positionV3;
+                meshObject.transform.localScale = Vector3.one * size / 10f;
+                SetVisible(false);
+            }
+            public void UpdateTerrainChunk()
+            {
+                float viewerDstFromNearestEdge = Mathf.Sqrt(bounds.SqrDistance(viwerPosition));
+                bool visible = viewerDstFromNearestEdge <= maxViewDst;
+                SetVisible(visible);
+            }
+            public void SetVisible(bool visible)
+            {
+                meshObject.SetActive(visible);
+            }
         }
     }
 }
